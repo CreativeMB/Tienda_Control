392320a045c88d5557016f87f85bf3f9a5532faa
TobiasM <ingcpu@gmail.com>
2024-07-24 13:42:08 -0500

    funcionalidades Aplicación Autenticación de Usuario(firebase) Gestión de Productos (Crear, Leer, Actualizar, Eliminar) Gestión de Pedidos (gestionamos los valores negativo positivos y lo mostramos en pantalla) Operaciones CRUD SQLite(matemáticas en la base de datos) Diseño y Usabilidad (super agil y liviana) Documentación y Comentarios (archivo y video de presentación) Exportación de base de datos .db (carpeta Descargas) Importación de base de datos .db (carpeta Descargas del movil) Exportación de base de datos a Excel Activity Perfil de usuario con (firebase personalizado) Buscador de Items dentro de la base de datos menu diálogo layou egreso total layou ingreso total creacion de codigo de acceso personalizado para que no use login agregar items personalizados para usar de forma recurrente

--- app/src/main/java/com/example/tiendacontrol/helper/BaseExporter.java
+++ app/src/main/java/com/example/tiendacontrol/helper/BaseExporter.java
@@ -17,11 +17,10 @@ public class BaseExporter {
     private static final String TAG = "BaseExporter";
     private Context context;
 
-    // Constructor que recibe el contexto de la aplicación
     public BaseExporter(Context context) {
         this.context = context;
-    }
 
+    }
     // Método para exportar la base de datos
     public void exportDatabase(String dbFileName) {
         if (!isStoragePermissionGranted()) {
@@ -29,9 +28,39 @@ public class BaseExporter {
             return;
         }
 
-        // Mostrar el ProgressDialog y ejecutar la exportación en segundo plano
-        ProgressDialogHelper.showProgressDialog(context, "Exportando base de datos...");
-        new ExportTask(context, dbFileName).execute();
+        Log.d(TAG, "Iniciando exportación de la base de datos.");
+
+        File dbFile = context.getDatabasePath(dbFileName);
+        if (!dbFile.exists()) {
+            Log.e(TAG, "El archivo de la base de datos no existe: " + dbFileName);
+            Toast.makeText(context, "El archivo de la base de datos no existe", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Log.d(TAG, "Ruta de la base de datos original: " + dbFile.getAbsolutePath());
+
+        File exportDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File exportFile = new File(exportDir, dbFile.getName());
+        Log.d(TAG, "Ruta de destino para la exportación: " + exportFile.getAbsolutePath());
+
+        try {
+            if (exportFile.exists()) {
+                boolean deleted = exportFile.delete();
+                if (deleted) {
+                    Log.d(TAG, "Archivo existente eliminado: " + exportFile.getAbsolutePath());
+                } else {
+                    Log.e(TAG, "Error al eliminar el archivo existente: " + exportFile.getAbsolutePath());
+                }
+            }
+
+            copyFile(dbFile, exportFile);
+            Log.d(TAG, "Base de datos exportada exitosamente a: " + exportFile.getAbsolutePath());
+            Toast.makeText(context, "Base de datos exportada correctamente a " + exportFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Error al exportar la base de datos: " + e.getMessage());
+            Toast.makeText(context, "Error al exportar la base de datos", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // Método para importar la base de datos
@@ -41,141 +70,77 @@ public class BaseExporter {
             return;
         }
 
-        // Mostrar el ProgressDialog y ejecutar la importación en segundo plano
-        ProgressDialogHelper.showProgressDialog(context, "Importando base de datos...");
-        new ImportTask(context, dbFileName).execute();
+        Log.d(TAG, "Iniciando importación de la base de datos desde la carpeta de descargas.");
+
+        // Obtener la carpeta de descargas del dispositivo
+        File downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        if (!downloadDir.exists()) {
+            Log.e(TAG, "La carpeta de descargas no existe: " + downloadDir.getAbsolutePath());
+            Toast.makeText(context, "La carpeta de descargas no existe", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Construir la ruta completa del archivo de la base de datos en la carpeta de descargas
+        File sourceFile = new File(downloadDir, dbFileName);
+        if (!sourceFile.exists()) {
+            Log.e(TAG, "El archivo de la base de datos no existe en la carpeta de descargas: " + sourceFile.getAbsolutePath());
+            Toast.makeText(context, "El archivo de la base de datos no existe en la carpeta de descargas", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.d(TAG, "Ruta del archivo a importar desde descargas: " + sourceFile.getAbsolutePath());
+
+        // Ruta de destino para la importación (la base de datos actual de la app)
+        File destFile = context.getDatabasePath(dbFileName);
+        Log.d(TAG, "Ruta de destino para la importación: " + destFile.getAbsolutePath());
+
+        try {
+            // Eliminar el archivo de base de datos existente si existe
+            if (destFile.exists()) {
+                boolean deleted = destFile.delete();
+                if (deleted) {
+                    Log.d(TAG, "Archivo existente eliminado: " + destFile.getAbsolutePath());
+                } else {
+                    Log.e(TAG, "Error al eliminar el archivo existente: " + destFile.getAbsolutePath());
+                }
+            }
+
+            // Copiar el archivo desde la carpeta de descargas a la ubicación de la base de datos de la app
+            copyFile(sourceFile, destFile);
+            Log.d(TAG, "Base de datos importada exitosamente desde descargas a: " + destFile.getAbsolutePath());
+            Toast.makeText(context, "Base de datos importada correctamente desde descargas", Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Error al importar la base de datos desde descargas: " + e.getMessage());
+            Toast.makeText(context, "Error al importar la base de datos desde descargas", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // Método para verificar si los permisos de almacenamiento están concedidos
     private boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // Para Android 11 y versiones posteriores
             return Environment.isExternalStorageManager();
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Para Android 6.0 a 10
             return ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED;
         } else {
-            // Para versiones anteriores a Android 6.0
+            // Versions prior to Android M do not need runtime permission
             return true;
         }
     }
 
-    // Tarea asíncrona para exportar la base de datos
-    private static class ExportTask extends AsyncTask<Void, Void, Boolean> {
-        private Context context;
-        private String dbFileName;
-
-        ExportTask(Context context, String dbFileName) {
-            this.context = context;
-            this.dbFileName = dbFileName;
+    // Método para copiar un archivo
+    private void copyFile(File sourceFile, File destFile) throws IOException {
+        FileInputStream fis = new FileInputStream(sourceFile);
+        FileOutputStream fos = new FileOutputStream(destFile);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = fis.read(buffer)) > 0) {
+            fos.write(buffer, 0, length);
         }
-
-        @Override
-        protected Boolean doInBackground(Void... voids) {
-            File dbFile = context.getDatabasePath(dbFileName);
-            if (!dbFile.exists()) {
-                Log.e(TAG, "El archivo de la base de datos no existe: " + dbFileName);
-                return false;
-            }
-
-            File exportDir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS); // Use internal storage directory
-            if (exportDir == null) {
-                Log.e(TAG, "El directorio de exportación no es accesible.");
-                return false;
-            }
-
-            File exportFile = new File(exportDir, dbFile.getName());
-
-            try {
-                if (exportFile.exists() && !exportFile.delete()) {
-                    Log.e(TAG, "Error al eliminar el archivo existente: " + exportFile.getAbsolutePath());
-                    return false;
-                }
-
-                copyFile(dbFile, exportFile);
-                return true;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e(TAG, "Error al exportar la base de datos: " + e.getMessage());
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            ProgressDialogHelper.dismissProgressDialog();
-            if (success) {
-                Toast.makeText(context, "Base de datos exportada correctamente a " + context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath(), Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(context, "Error al exportar la base de datos", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    // Tarea asíncrona para importar la base de datos
-    private static class ImportTask extends AsyncTask<Void, Void, Boolean> {
-        private Context context;
-        private String dbFileName;
-
-        ImportTask(Context context, String dbFileName) {
-            this.context = context;
-            this.dbFileName = dbFileName;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... voids) {
-            File downloadDir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS); // Use internal storage directory
-            if (downloadDir == null) {
-                Log.e(TAG, "El directorio de descargas no es accesible.");
-                return false;
-            }
-
-            File sourceFile = new File(downloadDir, dbFileName);
-            if (!sourceFile.exists()) {
-                Log.e(TAG, "El archivo de la base de datos no existe en la carpeta de descargas: " + sourceFile.getAbsolutePath());
-                return false;
-            }
-
-            File destFile = context.getDatabasePath(dbFileName);
-
-            try {
-                if (destFile.exists() && !destFile.delete()) {
-                    Log.e(TAG, "Error al eliminar el archivo existente: " + destFile.getAbsolutePath());
-                    return false;
-                }
-
-                copyFile(sourceFile, destFile);
-                return true;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e(TAG, "Error al importar la base de datos desde descargas: " + e.getMessage());
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            ProgressDialogHelper.dismissProgressDialog();
-            if (success) {
-                Toast.makeText(context, "Base de datos importada correctamente desde descargas", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(context, "Error al importar la base de datos desde descargas", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    // Método para copiar un archivo de un lugar a otro
-    private static void copyFile(File sourceFile, File destFile) throws IOException {
-        try (FileInputStream fis = new FileInputStream(sourceFile);
-             FileOutputStream fos = new FileOutputStream(destFile)) {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = fis.read(buffer)) > 0) {
-                fos.write(buffer, 0, length);
-            }
-        }
+        fos.flush();
+        fos.close();
+        fis.close();
     }
 }
